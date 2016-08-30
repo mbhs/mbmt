e2868067a71d29fc175ce00999550606e9c8d12e
Noah Singer <singerng@gmail.com>
2016-08-29 21:37:31 -0400

    Okay, had an extra 't'

--- mbmt.py
+++ mbmt.py
@@ -4,7 +4,7 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 
 # Default Flask configuration
 
-app = Flask(__name__)t
+app = Flask(__name__)
 app.config.from_object(__name__)
 
 app.config.update(dict(
