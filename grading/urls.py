b1bb1f2f18091f12b0847185f135709cf3b2bc34
Noah Kim <noahbkim@gmail.com>
2017-03-25 23:58:48 -0400

    Added a sick dropdown.

--- grading/urls.py
+++ grading/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     # Logistics
     url(r'^shirts/$', views.shirts, name="shirts"),
     url(r'^attendance/$', views.attendance, name="attendance"),
-    url(r'^tags/student/$', views.student_name_tags, name="student_name_tags"),
-    url(r'^tags/teacher/$', views.teacher_name_tags, name="teacher_name_tags"),
+    url(r'^tags/students/$', views.student_name_tags, name="student_name_tags"),
+    url(r'^tags/teachers/$', views.teacher_name_tags, name="teacher_name_tags"),
 
     # Scoring
     url(r'^scoreboard/$', views.scoreboard, name="scoreboard"),
