faa02d55b3bf31e931e6a4768871cb889d0f997a
Noah Kim <noahbkim@gmail.com>
2017-03-31 12:47:48 -0400

    Added freeze function.

--- frontend/static/js/guts.js
+++ frontend/static/js/guts.js
@@ -17,30 +17,41 @@ function request(url) {
 }
 
 var scoreboards;
+var frozen = false;
 
 function update() {
-    request("/live/guts/update/").then(function(scores) {
-        for (var division in scores) {
-            if (scores.hasOwnProperty(division)) {
-                var teams = [];
-                for (var team in scores[division])
-                    if (scores[division].hasOwnProperty(team))
-                        teams.push(team);
-                teams.sort(function(a, b) {
-                    return scores[division][b] - scores[division][a];
-                });
+    if (!frozen) {
+        request("/live/guts/update/").then(function (scores) {
+            for (var division in scores) {
+                if (scores.hasOwnProperty(division)) {
+                    var teams = [];
+                    for (var team in scores[division])
+                        if (scores[division].hasOwnProperty(team))
+                            teams.push(team);
+                    teams.sort(function (a, b) {
+                        return scores[division][b] - scores[division][a];
+                    });
 
-                while (scoreboards[division].firstChild)
-                    scoreboards[division].removeChild(scoreboards[division].firstChild);
-                for (var i = 0; i < teams.length; i++)
-                    scoreboards[division].insertRow().innerHTML += (
-                        "<td>" + (i+1) + "</td>" +
+                    while (scoreboards[division].firstChild)
+                        scoreboards[division].removeChild(scoreboards[division].firstChild);
+                    for (var i = 0; i < teams.length; i++)
+                        scoreboards[division].insertRow().innerHTML += (
+                        "<td>" + (i + 1) + "</td>" +
                         "<td class=\"team\">" + teams[i] + "</td>" +
-                        "<td>" + Math.round(scores[division][teams[i]]*1000)/1000 + "</td>");
+                        "<td>" + Math.round(scores[division][teams[i]] * 1000) / 1000 + "</td>");
+                }
             }
-        }
-        console.log("Updated scoreboard!");
-    });
+            console.log("Updated scoreboard!");
+        });
+    } else {
+        console.log("Didn't update!");
+    }
+}
+
+function freeze() {
+    frozen = !frozen;
+    if (frozen) document.getElementById("freeze").innerHTML = "Frozen!";
+    else document.getElementById("freeze").innerHTML = "Freeze!"
 }
 
 window.onload = function() {
