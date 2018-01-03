1b1fbbab887daeb350f89d00df9439c3df920b78
Noah Kim <noahbkim@gmail.com>
2018-01-03 08:59:09 -0500

    Fixed template refactor

--- home/views.py
+++ home/views.py
@@ -3,10 +3,10 @@ from functools import partial
 from django.shortcuts import render
 
 
-index = partial(render, template_name="index.html")
-info = partial(render, template_name="info.html")
-about = partial(render, template_name="about.html")
-archive = partial(render, template_name="archive.html")
-rules = partial(render, template_name="rules.html")
-topics = partial(render, template_name="topics.html")
-registration = partial(render, template_name="registration.html")
+index = partial(render, template_name="home/index.html")
+info = partial(render, template_name="home/info.html")
+about = partial(render, template_name="home/about.html")
+archive = partial(render, template_name="home/archive.html")
+rules = partial(render, template_name="home/rules.html")
+topics = partial(render, template_name="home/topics.html")
+registration = partial(render, template_name="home/registration.html")
